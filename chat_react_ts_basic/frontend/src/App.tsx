4c0999f3d45048f1bd47c6b3283f2e54ce1b4475
derickferreira <derickferreira036@gmail.com>
2024-08-23 15:32:14 +0100

    Doing the main.tsx, app.tsx and creating new pages. Some folders were removed too

--- chat_react_ts_basic/frontend/src/App.tsx
+++ chat_react_ts_basic/frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import "./App.css";
+
+// Dependencies
+import { Outlet } from "react-router-dom";
 
 function App() {
-
   return (
     <>
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- chat_react_ts_basic/frontend/src/main.tsx
+++ chat_react_ts_basic/frontend/src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// pages
+import ChatPage from "./pages/ChatPage.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+
+// Dependencies
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// context
+
+const routes = [
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/chat",
+    element: <ChatPage />,
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: routes,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
