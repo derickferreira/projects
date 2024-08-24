1aeffa1a7c6611daf6608f15d9fa84082cdb1e24
derickferreira <derickferreira036@gmail.com>
2024-08-24 10:16:27 +0100

    ChatMessageComponent was done

--- chat_react_ts_basic/frontend/src/components/ChatMessageComponent.tsx
+++ chat_react_ts_basic/frontend/src/components/ChatMessageComponent.tsx
@@ -1,13 +1,45 @@
-const ChatMessageComponent = () => {
+import { useContext } from "react";
+
+// context
+import { UserContext } from "../context/UserContext";
+
+// interfcae
+interface IMessage {
+  userId: string;
+  userName: string;
+  userColour: string;
+  message: string;
+  timestamp: number;
+}
+
+const ChatMessageComponent = ({ messages }: { messages: IMessage[] }) => {
+  const context = useContext(UserContext);
+  if (!context) return;
+  const { user } = context;
+
   return (
     <section className="chat">
       <section className="chat_messages">
-        <div className="message_self">Hello World!</div>
+        {messages.map((msg, index) => (
+          <div
+            key={index}
+            className={
+              msg.userId === user?.id ? "message_self" : "message_other"
+            }
+          >
+            <span className="message_sender" style={{ color: msg.userColour }}>
+              {msg.userId == user?.id ? "you" : msg.userName}
+            </span>
+            {msg.message}
+          </div>
+        ))}
+
+        {/* <div className="message_self">Hello World!</div>
 
         <div className="message_other">
           <span className="message_sender">Derick</span>
           Hello World
-        </div>
+        </div> */}
       </section>
     </section>
   );
