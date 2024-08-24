8da53269434a845d0612ec8db73ff1f0a120c333
derickferreira <derickferreira036@gmail.com>
2024-08-24 09:29:02 +0100

    Backend was done

--- chat_react_ts_basic/backend/build/index.js
+++ chat_react_ts_basic/backend/build/index.js
@@ -1,2 +1,17 @@
-"use strict";
+import { WebSocketServer } from "ws";
+import "dotenv/config";
+const port = process.env.PORT ? +process.env.PORT : 8080;
+const wss = new WebSocketServer({ port });
+wss.on("connection", (ws) => {
+    ws.on("close", (err) => {
+        console.log(`Connection closed: ${err}`);
+    });
+    ws.on("message", (data) => {
+        console.log(data.toString());
+        wss.clients.forEach((client) => {
+            client.send(data.toString());
+        });
+    });
+    console.log("New connection");
+});
 //# sourceMappingURL=index.js.map
---
+++ chat_react_ts_basic/backend/src/index.ts
@@ -0,0 +1,21 @@
+import { WebSocketServer } from "ws";
+import "dotenv/config";
+
+const port: number = process.env.PORT ? +process.env.PORT : 8080;
+
+const wss = new WebSocketServer({ port });
+
+wss.on("connection", (ws) => {
+  ws.on("close", (err) => {
+    console.log(`Connection closed: ${err}`);
+  });
+
+  ws.on("message", (data) => {
+    console.log(data.toString());
+    wss.clients.forEach((client) => {
+      client.send(data.toString());
+    });
+  });
+
+  console.log("New connection");
+});
