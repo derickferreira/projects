f03e10463edcda4b4cf920cc9fea8388823171d1
derickferreira <derickferreira036@gmail.com>
2024-08-24 09:58:07 +0100

    LoginPage was done

--- chat_react_ts_basic/frontend/src/pages/LoginPage.tsx
+++ chat_react_ts_basic/frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,69 @@
+import { FormEvent, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "./../context/UserContext";
+
+// types
+type colour = Array<string>;
+
+// interface
+interface IUser {
+  id: string;
+  name: string;
+  colour: string;
+}
+
+// variables, objects or arrays
+
+const colours: colour = [
+  "cadetblue",
+  "darkgoldenrod",
+  "cornflowerblue",
+  "hotpink",
+  "gold",
+  "aqua",
+  "darkgreen",
+  "midnightblue",
+];
+
+// functions or logic
+const randomColour = (colours: colour): string => {
+  return colours[Math.floor(Math.random() * colours.length)];
+};
+
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const context = useContext(UserContext);
+  if (!context)
+    throw new Error("UserContext must be used within a UserProvider");
+
+  const [nickname, setNickname] = useState<string>("");
+  const { setUser } = context;
+
+  const handleLogin = (event: FormEvent): void => {
+    event.preventDefault();
+
+    const newUser: IUser = {
+      id: crypto.randomUUID(),
+      name: nickname,
+      colour: randomColour(colours),
+    };
+
+    setUser(newUser);
+    setNickname("");
+
+    navigate("/chat");
+  };
+
   return (
     <section className="login">
       <h2>Login</h2>
-      <form className="login_form">
+      <form className="login_form" onSubmit={handleLogin}>
         <input
           type="text"
           className="login_input"
           placeholder="nickname"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
           required
         />
         <button type="submit" className="login_button">
