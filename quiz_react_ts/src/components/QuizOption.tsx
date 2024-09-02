cf344f23d54ee6548df282dc3f4e443423ba0380
derickferreira <derickferreira036@gmail.com>
2024-09-02 10:34:26 +0100

    Bugs fixed

--- quiz_react_ts/src/components/QuizOption.tsx
+++ quiz_react_ts/src/components/QuizOption.tsx
@@ -2,6 +2,7 @@ import { useQuizContext } from "./../contexts/QuizContext";
 
 const QuizOption = ({ option }: { option: string }) => {
   const { handleAnswer } = useQuizContext();
+
   return (
     <div className="answer_container">
       <button onClick={() => handleAnswer(option)}>{option}</button>
--- quiz_react_ts/src/main.tsx
+++ quiz_react_ts/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/style.css";
@@ -7,9 +6,9 @@ import "./styles/style.css";
 import { QuizProvider } from "./contexts/QuizContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <>
     <QuizProvider>
       <App />
     </QuizProvider>
-  </StrictMode>
+  </>
 );
